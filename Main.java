f6eb5fcc1659f4ec77db67286e984493a4b27e70
JavokhirKhasanov <javohir23.09.2017@gmail.com>
2023-01-07 20:04:07 +0500

    sum funksion

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(sum(7,8));
+    }
+    public static Integer sum(int a,int b){
+        return a+b;
+    }
+}
